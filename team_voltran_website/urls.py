a35a3e5e1984f7f84646943930689e9d817beefc
DrShahinstein <1taylanozturk@protonmail.com>
2023-12-09 11:40:50 +0300

    Remove api endpoints from urlpatterns

--- team_voltran_website/urls.py
+++ team_voltran_website/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import IndexView, MembersView, ProjectsView, LogsView
+from core.views import IndexView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="main-page"),
-    path("api/members/", MembersView.as_view(), name="GET members"),
-    path("api/projects/", ProjectsView.as_view(), name="GET projects"),
-    path("api/logs/", LogsView.as_view(), name="GET logs"),
 ]
